bc6aa9a5e2ab42abf5a6af0bf874697b72bc782c
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-05-12 14:09:49 +0900

    データ一覧画面作成

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("../models/index");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", (req, res, next) => {
+  db.User.findAll().then((users) => {
+    console.dir(JSON.stringify(db.User.findAll()));
+    const data = {
+      title: "Users/Index",
+      content: users,
+    };
+    res.render("users/index", data);
+  });
 });
 
 module.exports = router;
